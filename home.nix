4e32d5d9b25e17a4d8cd2675654eefd936159f1d
Ivan Alvarez <20419834+ivanovertime@users.noreply.github.com>
2026-06-18 10:35:18 -0400

    feat(configuration): remove unused configuration files for kitty, neovim, ranger, tmux, and packages

--- home.nix
+++ home.nix
@@ -1,14 +1,6 @@
 { pkgs, ... }:
 
 {
-  imports = [
-    ./home/packages.nix
-    ./home/neovim.nix
-    ./home/kitty.nix
-    ./home/ranger.nix
-    ./home/tmux.nix
-  ];
-
   home.username = "ivan";
   home.homeDirectory = "/home/ivan";
   home.stateVersion = "25.05";
