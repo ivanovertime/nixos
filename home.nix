0466ed54b569187820b4f4793c7b77e6537859df
Ivan Alvarez <20419834+ivanovertime@users.noreply.github.com>
2026-07-04 10:20:35 -0400

    feat(configuration): add tmux configuration and enhance yazi settings with Everforest theme

--- home.nix
+++ home.nix
@@ -106,6 +106,23 @@ in
   xdg.dataFile."icons/Tela-circle-green-dark/scalable/apps/org.gnome.Loupe.svg".source =
     "${pkgs.tela-circle-icon-theme.override { colorVariants = [ "green" ]; }}/share/icons/Tela-circle-green-dark/scalable/apps/accessories-image-viewer.svg";
 
+  programs.tmux = {
+    enable = true;
+    extraConfig = ''
+      set -g mouse on
+      set -g history-limit 100000
+      set -g status-interval 5
+      set -g status-justify centre
+      set -g status-left-length 40
+      set -g status-right-length 90
+      set -g status-left "#[fg=green]#H"
+      set -g status-right "#[fg=yellow]%Y-%m-%d %H:%M:%S"
+      set -g allow-passthrough on
+      set -ga update-environment TERM
+      set -ga update-environment TERM_PROGRAM
+    '';
+  };
+
   programs.zellij = {
     enable = true;
     settings = {
@@ -116,6 +133,42 @@ in
   programs.yazi = {
     enable = true;
     shellWrapperName = "yy";
+    extraPackages = with pkgs; [
+      chafa
+    ];
+
+    flavors = {
+      "everforest-medium" = pkgs.fetchFromGitHub {
+        owner = "Chromium-3-Oxide";
+        repo = "everforest-medium.yazi";
+        rev = "e1ead7b5a3bfc8eb572fd269a369775842752705";
+        hash = "sha256-2Fx7+xnSsc+aVHBZUtLtVUDEzb1y8BcPBASciKk8x7o=";
+      };
+    };
+
+    settings = {
+      mgr = {
+        ratio = [ 1 4 3 ];
+        sort_dir_first = true;
+        show_hidden = false;
+        show_symlink = true;
+      };
+
+      preview = {
+        wrap = "yes";
+        tab_size = 4;
+        max_width = 8192;
+        max_height = 8192;
+        image_filter = "lanczos3";
+        image_quality = 90;
+      };
+    };
+
+    theme = {
+      flavor = {
+        dark = "everforest-medium";
+      };
+    };
   };
   
   programs.eza = {
@@ -132,6 +185,8 @@ in
   programs.bash = {
     enable = true;
     shellAliases = {
+      yazi = "yazi";
+      yy = "yazi";
       ls = "eza --icons=always --group-directories-first";
       ll = "eza --long --git --icons=always --group-directories-first --header";
       la = "eza --all --icons=always --group-directories-first";
