5c0c1956ff871ccac8e3780ef796281089119c9f
Ivan Alvarez <20419834+ivanovertime@users.noreply.github.com>
2026-08-03 15:25:15 -0400

    refactor(emacs): keep init config in init.el

--- home/editors/emacs.nix
+++ home/editors/emacs.nix
@@ -18,38 +18,9 @@
         which-key
       ];
 
-    # This is injected directly into your Emacs init file (early-init.el / init.el)
-    extraConfig = ''
-      ;; Disable startup message
-      (setq inhibit-startup-message t)
-
-      ;; Disable GUI cruft for a cleaner look
-      (tool-bar-mode -1)
-      (menu-bar-mode -1)
-      (scroll-bar-mode -1)
-
-      ;; Enable line numbers globally
-      (global-display-line-numbers-mode t)
-
-      ;; Load the doom-zenburn theme (ensure it's in extraPackages above)
-      (load-theme 'doom-zenburn t)
-
-      ;; Enhanced minibuffer completion (better M-x)
-      (savehist-mode 1)
-      (vertico-mode 1)
-      (marginalia-mode 1)
-
-      ;; Show available keybindings after a short delay
-      (which-key-mode 1)
-
-      ;; Maximize frame on startup
-      (add-to-list 'initial-frame-alist '(fullscreen . maximized))
-
-      ;; Doom-style landing screen
-      (require 'dashboard)
-      (dashboard-setup-startup-hook)
-      (setq dashboard-startup-banner 'logo)
-    '';
+    # Kept in ./emacs/init.el so it's version-controlled and diffable like
+    # everything else in this repo.
+    extraConfig = builtins.readFile ./emacs/init.el;
   };
 
   services.emacs = {
