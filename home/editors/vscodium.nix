2cd7c6939e146fc31e7b29c5d3791c6c20f7b6b2
Ivan Alvarez <20419834+ivanovertime@users.noreply.github.com>
2026-08-04 14:32:48 -0400

    feat(vscodium): stop managing settings.json and add OpenChamber
    Leave settings.json unmanaged (user-owned file already disables update checks via update.mode and extensions.autoCheckUpdates), and add the OpenChamber extension, packaged from Open VSX since it is not yet in nixpkgs.

--- home/editors/vscodium.nix
+++ home/editors/vscodium.nix
@@ -10,39 +10,112 @@
     mutableExtensionsDir = true;
 
     profiles.default = {
-      # Keep update checks disabled because this install is managed declaratively.
-      enableUpdateCheck = false;
-      enableExtensionUpdateCheck = false;
+      # settings.json is left unmanaged (user-owned file); update checks are
+      # disabled via that file (update.mode = "none", extensions.autoCheckUpdates = false).
+      # Keep these two true so home-manager does not generate settings.json.
 
       extensions =
         let
           ext = pkgs.vscode-extensions;
-          optionalExt = path:
-            if pkgs.lib.hasAttrByPath path ext
-            then [ (pkgs.lib.attrByPath path null ext) ]
-            else [ ];
+          optionalExt =
+            path: if pkgs.lib.hasAttrByPath path ext then [ (pkgs.lib.attrByPath path null ext) ] else [ ];
         in
         builtins.concatLists [
-          (optionalExt [ "bierner" "github-markdown-preview" ])
-          (optionalExt [ "bierner" "markdown-checkbox" ])
-          (optionalExt [ "bierner" "markdown-emoji" ])
-          (optionalExt [ "bierner" "markdown-footnotes" ])
-          (optionalExt [ "bierner" "markdown-mermaid" ])
-          (optionalExt [ "bierner" "markdown-preview-github-styles" ])
-          (optionalExt [ "dbaeumer" "vscode-eslint" ])
-          (optionalExt [ "elixir-lsp" "vscode-elixir-ls" ])
-          (optionalExt [ "file-icons" "file-icons" ])
-          (optionalExt [ "github" "vscode-github-actions" ])
-          (optionalExt [ "jnoortheen" "nix-ide" ])
-          (optionalExt [ "jock" "svg" ])
-          (optionalExt [ "maciejdems" "add-to-gitignore" ])
-          (optionalExt [ "mkhl" "direnv" ])
-          (optionalExt [ "piotrpalarz" "vscode-gitignore-generator" ])
-          (optionalExt [ "redhat" "vscode-yaml" ])
-          (optionalExt [ "tomoki1207" "pdf" ])
-          (optionalExt [ "vue" "volar" ])
-          (optionalExt [ "yinfei" "luahelper" ])
-          (optionalExt [ "yzhang" "markdown-all-in-one" ])
+          # OpenChamber (openchamber/openchamber) is not in nixpkgs.vscode-extensions
+          # yet, so package the Open VSX release declaratively.
+          [
+            (pkgs.vscode-utils.buildVscodeExtension {
+              pname = "openchamber";
+              version = "1.18.1";
+              vscodeExtPublisher = "FedaykinDev";
+              vscodeExtName = "openchamber";
+              vscodeExtUniqueId = "FedaykinDev.openchamber";
+              src = pkgs.fetchurl {
+                url = "https://open-vsx.org/api/FedaykinDev/openchamber/1.18.1/file/FedaykinDev.openchamber-1.18.1.vsix";
+                sha256 = "sha256-RL9VNH22Fg7+9gomOu3lpJjyIxgkbUaBb92CT5EBl6M=";
+              };
+            })
+          ]
+          (optionalExt [
+            "bierner"
+            "github-markdown-preview"
+          ])
+          (optionalExt [
+            "bierner"
+            "markdown-checkbox"
+          ])
+          (optionalExt [
+            "bierner"
+            "markdown-emoji"
+          ])
+          (optionalExt [
+            "bierner"
+            "markdown-footnotes"
+          ])
+          (optionalExt [
+            "bierner"
+            "markdown-mermaid"
+          ])
+          (optionalExt [
+            "bierner"
+            "markdown-preview-github-styles"
+          ])
+          (optionalExt [
+            "dbaeumer"
+            "vscode-eslint"
+          ])
+          (optionalExt [
+            "elixir-lsp"
+            "vscode-elixir-ls"
+          ])
+          (optionalExt [
+            "file-icons"
+            "file-icons"
+          ])
+          (optionalExt [
+            "github"
+            "vscode-github-actions"
+          ])
+          (optionalExt [
+            "jnoortheen"
+            "nix-ide"
+          ])
+          (optionalExt [
+            "jock"
+            "svg"
+          ])
+          (optionalExt [
+            "maciejdems"
+            "add-to-gitignore"
+          ])
+          (optionalExt [
+            "mkhl"
+            "direnv"
+          ])
+          (optionalExt [
+            "piotrpalarz"
+            "vscode-gitignore-generator"
+          ])
+          (optionalExt [
+            "redhat"
+            "vscode-yaml"
+          ])
+          (optionalExt [
+            "tomoki1207"
+            "pdf"
+          ])
+          (optionalExt [
+            "vue"
+            "volar"
+          ])
+          (optionalExt [
+            "yinfei"
+            "luahelper"
+          ])
+          (optionalExt [
+            "yzhang"
+            "markdown-all-in-one"
+          ])
         ];
     };
   };
