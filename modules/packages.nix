fb38c3ff7431e610d1c569b9ef1fed6a2bd29536
Ivan Alvarez <20419834+ivanovertime@users.noreply.github.com>
2026-09-02 07:33:52 -0400

    feat(packages): remove firefox, thunderbird, postgresql and gimp

--- modules/packages.nix
+++ modules/packages.nix
@@ -7,16 +7,12 @@ in
   environment.systemPackages = with pkgs; [
     # Browsers
     pkgs-unstable.google-chrome
-    pkgs-unstable.firefox
-    pkgs-unstable.thunderbird
 
     # Development
-    postgresql
     pkgs-unstable.dbeaver-bin
 
     # Media & Graphics
     gthumb
-    gimp
     loupe
     pkgs-unstable.inkscape
     obs-studio
