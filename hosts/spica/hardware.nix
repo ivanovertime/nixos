1d7e6ba45f8b095d280d131fb200073571da535d
Ivan Alvarez <20419834+ivanovertime@users.noreply.github.com>
2026-09-02 07:33:48 -0400

    refactor(spica): drop redundant amd microcode option

--- hosts/spica/hardware.nix
+++ hosts/spica/hardware.nix
@@ -41,5 +41,4 @@
   swapDevices = [ ];
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
